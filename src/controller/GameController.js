6135dc7c7098337a8ebdc4667c1aa4a8e0de8238
heejaykong <heejay.dev@gmail.com>
2021-12-17 14:12:30 +0900

    refactor: controller 용도에 맞게 handler코드 재배치

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,14 +1,49 @@
-import { $, ID } from '../utils/constants.js';
-import { template } from '../view/templates/carNamesInputTemplate.js';
-import { handleCarNamesSubmit, clearInput } from '../view/InputView.js';
+import { $, ID, ERROR_MSG } from '../utils/constants.js';
+import { template as carNamesInputTemplate } from '../view/templates/carNamesInputTemplate.js';
+import { template as racingCountInputTemplate } from '../view/templates/racingCountInputTemplate.js';
+// import { handleCarNamesSubmit, clearInput } from '../view/InputView.js';
+import { validateCarNames, validateCountInput } from '../utils/validations.js';
+import { createCars } from '../utils/createCars.js';
 
 export default class GameController {
   constructor() {
+    this.names = [];
     this.init();
   }
 
   init = () => {
-    $(`#${ID.APP}`).innerHTML = template;
-    $(`#${ID.CAR_NAMES_SUBMIT}`).addEventListener('click', handleCarNamesSubmit);
+    $(`#${ID.APP}`).innerHTML = carNamesInputTemplate;
+    $(`#${ID.CAR_NAMES_SUBMIT}`).addEventListener('click', this.handleCarNamesSubmit);
+  };
+
+  handleCarNamesSubmit = (e) => {
+    e.preventDefault();
+    const carNamesInput = $(`#${ID.CAR_NAMES_INPUT}`).value;
+    const splitNames = carNamesInput.split(',').map((name) => name.trim());
+    const isValid = validateCarNames(splitNames);
+    if (!isValid) {
+      alert(ERROR_MSG.CAR_NAMES_SUBMIT);
+    }
+    if (isValid) {
+      this.names = splitNames;
+      racingCountInputTemplate();
+      $(`#${ID.RACING_COUNT_SUBMIT}`).addEventListener(
+        'click',
+        this.handleRacingCountSubmit
+      );
+    }
+  };
+  handleRacingCountSubmit = (e) => {
+    e.preventDefault();
+    const racingCountInput = $(`#${ID.RACING_COUNT_INPUT}`).value;
+    const isValid = validateCountInput(racingCountInput);
+    if (!isValid) {
+      alert(ERROR_MSG.RACING_COUNT_SUBMIT);
+    }
+    if (isValid) {
+      console.log(this.names);
+      const cars = createCars(this.names);
+      console.log(cars);
+    }
   };
 }
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,34 +5,34 @@ import { template } from './templates/racingCountInputTemplate.js';
 
 let names = [];
 
-export const handleCarNamesSubmit = (e) => {
-  e.preventDefault();
-  const carNamesInput = $(`#${ID.CAR_NAMES_INPUT}`).value;
-  const splitNames = carNamesInput.split(',').map((name) => name.trim());
-  const isValid = validateCarNames(splitNames);
-  if (!isValid) {
-    alert(ERROR_MSG.CAR_NAMES_SUBMIT);
-  }
-  if (isValid) {
-    names = splitNames;
-    template();
-    $(`#${ID.RACING_COUNT_SUBMIT}`).addEventListener('click', handleRacingCountSubmit);
-  }
-};
+// export const handleCarNamesSubmit = (e) => {
+//   e.preventDefault();
+//   const carNamesInput = $(`#${ID.CAR_NAMES_INPUT}`).value;
+//   const splitNames = carNamesInput.split(',').map((name) => name.trim());
+//   const isValid = validateCarNames(splitNames);
+//   if (!isValid) {
+//     alert(ERROR_MSG.CAR_NAMES_SUBMIT);
+//   }
+//   if (isValid) {
+//     names = splitNames;
+//     template();
+//     $(`#${ID.RACING_COUNT_SUBMIT}`).addEventListener('click', handleRacingCountSubmit);
+//   }
+// };
 
-export const handleRacingCountSubmit = (e) => {
-  e.preventDefault();
-  const racingCountInput = $(`#${ID.RACING_COUNT_INPUT}`).value;
-  const isValid = validateCountInput(racingCountInput);
-  if (!isValid) {
-    alert(ERROR_MSG.RACING_COUNT_SUBMIT);
-  }
-  if (isValid) {
-    console.log(names);
-    const cars = createCars(names);
-    console.log(cars);
-  }
-};
+// export const handleRacingCountSubmit = (e) => {
+//   e.preventDefault();
+//   const racingCountInput = $(`#${ID.RACING_COUNT_INPUT}`).value;
+//   const isValid = validateCountInput(racingCountInput);
+//   if (!isValid) {
+//     alert(ERROR_MSG.RACING_COUNT_SUBMIT);
+//   }
+//   if (isValid) {
+//     console.log(names);
+//     const cars = createCars(names);
+//     console.log(cars);
+//   }
+// };
 
 export const clearInput = ($element) => {
   $element.value = '';
